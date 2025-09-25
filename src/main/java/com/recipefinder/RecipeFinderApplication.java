3da494ccbe2dc7c669293df5e359826a49736f51
kumbharrohit177-png <kumbharrohit177@gmail.com>
2025-09-25 22:20:51 +0530

    Update RecipeFinderApplication.java

--- src/main/java/com/recipefinder/RecipeFinderApplication.java
+++ src/main/java/com/recipefinder/RecipeFinderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  main {
+public class  RecipeFinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipeFinderApplication.class, args);
     }
